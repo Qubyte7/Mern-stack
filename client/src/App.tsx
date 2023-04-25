5a92a1b4bd80345e32d15e1fa9757ca66ab10d7a
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-25 17:01:41 +0200

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>
+
+        </h1>
+
+        </div>
+    </>
+  )
+}
+
+export default App
