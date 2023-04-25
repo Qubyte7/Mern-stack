2b13758560b1715fba2eeea82e76c8cd9fd06cd4
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-25 17:10:50 +0200

    installing nedded packages

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
 const express= require('express');
+const dotenv =require('dotenv').config()
+const app = express()
+const Port = process.env.PORT
 
+
+app.listen(Port,()=>{
+    console.log(`App running on Port ${Port}`)
+})
 
 
