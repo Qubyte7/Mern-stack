ee711184700084dfa0615f6c4939e9a8c6ed577a
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-26 06:03:05 +0200

    connecting the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express= require('express');
 const dotenv =require('dotenv').config()
 const app = express()
+const mongoose = require('mongoose')
 const Port = process.env.PORT
 const workoutRoutes = require('./Routes/workouts')
+const database = process.env.DATABASE;
 
 
 app.use((req,res,next)=>{
@@ -11,10 +13,15 @@ app.use((req,res,next)=>{
 })
     app.use(express.json())//see if any request coming have a body and if it does it passes it to the request Body so we can access it by a request handeler 
 app.use('/api/workouts',workoutRoutes)
-//routes
 
 
-app.listen(Port,()=>{
-    console.log(`App running on Port ${Port}`)
+//connect to db
+mongoose.connect(database)
+.then(()=>{
+    app.listen(Port,()=>{
+        console.log(`App running on Port ${Port} and connected to the database`);
+    })
+        
 })
-
+.catch((error)=>{console.log(error)})
+   
