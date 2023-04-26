ba78ae96c26f4732c00db5cdc62bcd3d59be31f0
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-26 06:46:45 +0200

    Done with the model

--- backend/Routes/workouts.js
+++ backend/Routes/workouts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const WorkoutModel = require('../models/workoutModel')
 
 router.get('/',(req,res)=>{
   res.json({msg:"get all workouts"})
@@ -10,9 +10,16 @@ router.get('/:id',(req,res)=>{
     res.json({msg:"Getting a single workout"})
 })
 
-router.post('/',(req,res)=>{
+router.post('/',async(req,res)=>{
+    const {title,reps,load} = req.body
+    try{
+        const workout=await WorkoutModel.create()
+    }
+    catch(error){}
    res.json({msg:"Creating a new workout"})
 })
+
+
 router.delete('/:id',(req,res)=>{
     res.json({msg:"deleting a workou"});
 })
