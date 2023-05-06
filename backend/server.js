5a8fab01847f5aceb9e0f4dcbf4123726249d02a
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-05-06 18:25:37 +0200

    backend done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv =require('dotenv').config()
 const app = express()
 const mongoose = require('mongoose')
 const Port = process.env.PORT
+const cors = require('cors')
 const workoutRoutes = require('./Routes/workouts')
 const database = process.env.DATABASE;
 
@@ -11,7 +12,8 @@ app.use((req,res,next)=>{
     console.log(req.path,req.method)
     next()
 })
-    app.use(express.json())//see if any request coming have a body and if it does it passes it to the request Body so we can access it by a request handeler 
+app.use(cors())
+app.use(express.json())//see if any request coming have a body and if it does it passes it to the request Body so we can access it by a request handeler 
 app.use('/api/workouts',workoutRoutes)
 
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react"
+import  Axios  from 'axios'
  const Home = ()=>{
+    const [workouts,setworkouts] = useState(null)
+    // useEffect(()=>{
+    //     const fetchWorkout = async ()=>{
+    //     const response = await fetch('http://localhost:5000/api/workouts')
+    //      const json = await response.json()
+    //      if(response.ok){
+
+    //      }
+    //     }
+
+    //     fetchWorkout()
+    // },[])
+    
+    const fetching = async ()=>{
+        Axios.get('http://localhost:8080/api/workouts')
+        .then((response)=>{
+            setworkouts(response.data)
+            console.log(response.data)})
+        .catch((err)=>{console.log(err)})
+    }
+
+    useEffect(()=>{
+     fetching()
+    },[])
+    
+    
     return(
         <div>
+
             <h2>Home</h2>
+            
         </div>
     )
 }
-
+ 
 export default Home
