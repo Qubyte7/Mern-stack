a23d5799371aee3fa2a6ef6d0dab8f9b847c5c09
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-05-17 15:01:23 +0200

    Done with token provision  and email validation

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,11 @@
 const User= require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
+
+const createToken = (_id) =>{
+return  jwt.sign({_id},process.env.SECRET)//_id means _id: _id
+
+}
 
 //login user
 const loginUser = async(req,res)=>{
@@ -9,8 +15,11 @@ res.json({mssg: 'LOgin user'})}
 const signUser = async(req,res)=>{
 const {email,password}= req.body
 try{
-const user = await User.signup(email,password)
-res.status(200).json({email,user})
+const user = await User.signup(email,password);
+const token = createToken(user._id)
+
+
+res.status(200).json({email, token})
 }catch(err){
 res.status(400).json({error:err.message })
 } 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const mongoose =require('mongoose');
+const validator =require('validator');
 
 
 const Schema =  mongoose.Schema
@@ -13,12 +14,27 @@ const userSchema = new Schema({
     password:{
         type:String,
         required:true,
+        
     }
 })
+
 //static signup method
 //Note:this was used to reference to the model other wise we would use "User" but because at this time there is no user created we have to use this
-// and notice that we will use the normal function not an arro function
+// and notice that we will use the normal function not an arro functions
+
 userSchema.statics.signup = async function (email,password) {
+   //validTION
+   if(!email || !password){
+    throw Error('all fields must be filled')
+   }
+   if(!validator.isEmail(email)){
+      throw Error('Email is not valid');
+   }
+   
+//    if(!validator.isStrongPassword(password)){
+//      throw Error("password not strong enough");
+//    }
+
         const exist =  await this.findOne({email})//this point or referrs to the model of the user           
         if(exist){
             throw Error('Email already in use')
